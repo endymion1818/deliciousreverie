f78c451ea7f6c4e7e6a55939fdcfc667bac5c046
ben-read <ben.read@zopa.com>
2020-02-24 15:38:38 +0000

    component init

--- src/components/Organisms/rabbits.tsx
+++ src/components/Organisms/rabbits.tsx
@@ -2,5 +2,5 @@ import React from "react";
 import TreeScene from "../../assets/tree-scene.svg";
 
 export default function rabbits() {
-  return <aside class="tree-scene" />;
+  return <aside className="tree-scene" />;
 }
